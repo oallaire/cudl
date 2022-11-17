1389e56f9ebcb9a74eae8e77ba4801ecf46a45e7
Olivier Allaire <olivier.allaire@gmail.com>
2022-11-17 17:09:59 -0500

    First proposition of library

--- lib/cudl.h
+++ lib/cudl.h
@@ -1,8 +1,41 @@
-//
-// Created by oallaire on 17/11/22.
-//
+#ifndef CUDL_H
+#define CUDL_H
 
-#ifndef UNTITLED_CUDL_H
-#define UNTITLED_CUDL_H
+#ifdef __cplusplus
+extern "C"
+{
+#endif
 
-#endif //UNTITLED_CUDL_H
+#define __CUDLE_UT(_name) _name##_t // NOLINT(bugprone-reserved-identifier)
+#define __CUDLE_NOP(_x) _x // NOLINT(bugprone-reserved-identifier)
+#define __CUDLE_FACTOR(_factor) (_x // NOLINT(bugprone-reserved-identifier)
+
+#define CUDL_ADD_UNIT_WITH_OP(_name, _type, _op)              \
+typedef struct _name##_s { _type value; } __CUDLE_UT(_name);  \
+static inline __CUDLE_UT(_name) _name(_type input_value) {    \
+    __CUDLE_UT(_name) ct;                                     \
+    ct.value = _op(input_value);                              \
+    return ct;                                                \
+}
+
+#define CUDL_ADD_UNIT(_name, _type) CUDL_ADD_UNIT_WITH_OP(_name, _type, __CUDLE_NOP)
+
+#define CUDL_GET(_value) (_value).value
+
+#define CUDL_ADD_EXPLICIT_CONVERSION_FRACTION_FACTOR(_from, _to, _explicative, _n, _d)       \
+static inline __CUDLE_UT(_to) _explicative##_to(__CUDLE_UT(_from) from_value) {              \
+    __CUDLE_UT(_to) to_value;                                                                \
+    to_value.value = (from_value.value * _n) / _d;                                           \
+    return to_value;                                                                         \
+}
+
+#define CUDL_ADD_CONVERSION_FRACTION_FACTOR(_from, __to, _n, _d) \
+CUDL_ADD_EXPLICIT_CONVERSION_FRACTION_FACTOR(_from, __to, from##_from##_to, _n, _d)
+
+#define CUDL_ADD_CONVERSION_FACTOR(_from, _to, _factor) CUDL_ADD_CONVERSION_FRACTION_FACTOR(_from, _to, _factor, 1)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //CUDL_H
---
+++ test/cudl_test.cpp
@@ -0,0 +1,22 @@
+#include <gtest/gtest.h>
+#include <cudl.h>
+#include <cstdint>
+
+CUDL_ADD_UNIT(_v, uint32_t)
+CUDL_ADD_UNIT(_mv, uint32_t)
+
+CUDL_ADD_CONVERSION_FACTOR(_v, _mv, 1000)
+CUDL_ADD_CONVERSION_FRACTION_FACTOR(_mv, _v, 1, 1000)
+
+TEST(test_cudl, whenUsingNopUnits_valueRemainsTheSame)
+{
+    _v_t volts = _v(10);
+    ASSERT_EQ(CUDL_GET(volts), 10);
+}
+
+TEST(test_cudl, whenUsingVolts_canConvertToMillivolts)
+{
+    _v_t volts = _v(10);
+    _mv_t mvolts = from_v_to_mv(volts);
+    ASSERT_EQ(CUDL_GET(mvolts), 10000);
+}
